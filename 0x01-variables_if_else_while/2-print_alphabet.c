ae026193284c84a6728be79e43f5980194c161a8
Eshiema <eshiemaoshioke@gmail.com>
2023-06-19 16:00:47 -0700

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char m;
+	m = 'n';
+	while
+		(m <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar("\n");
+	return (0);
