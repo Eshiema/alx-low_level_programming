24ffead93f119a3f1a111a41ff603567415df7a8
Eshiema <eshiemaoshioke@gmail.com>
2023-06-19 17:16:25 -0700

    Singlr digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+	putchar('\n');
+	return (0);
+}
