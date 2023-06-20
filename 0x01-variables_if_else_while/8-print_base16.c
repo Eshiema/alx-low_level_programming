fc299615b74273ca889ea52f278e0cfe40eb90d6
Eshiema <eshiemaoshioke@gmail.com>
2023-06-19 17:35:26 -0700

    Numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+	int c;
+	b = 'a';
+	c = 0;
+	while
+		(c < 10) {
+		putchar(c + '0');
+		c++;
+		}
+	while
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
+
