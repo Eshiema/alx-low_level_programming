4061f0147182cd4a8ec82357ffee1fd65f4e22d0
Eshiema <eshiemaoshioke@gmail.com>
2023-06-26 09:29:23 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: string to print
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (i = longi; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
